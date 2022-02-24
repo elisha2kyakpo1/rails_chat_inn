9a4287585d17975abf9785826fd8649661ffd0d1
elisha2kyakpo1 <kyakopoe@gmail.com>
2022-02-24 11:26:14 +0200

    Add rich text to message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,6 @@
 class Message < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, depedent: :destroy
+  has_rich_text :content
+  belongs_to :parent, class_name: 'Message', optional: true
+  validates :content, presence: true
 end
