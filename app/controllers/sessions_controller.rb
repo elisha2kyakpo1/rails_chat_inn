0b2da9ef1af9472e97a6cedc8a13c804199a7bec
elisha2kyakpo1 <kyakopoe@gmail.com>
2022-02-28 13:27:24 +0200

    Dugging

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,15 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     if user
-      session[:author_id] = user.id
-      redirect_to posts_path, notice: 'Logged in succussfully!'
+      session[:user_id] = user.id
+      redirect_to messages_path, notice: 'Logged in succussfully!'
     else
       render 'new', alert: @user.errors.full_messages.join('. ').to_s
     end
   end
 
   def logout_user
-    session[:author_id] = nil
+    session[:user_id] = nil
     redirect_to root_path, notice: 'Logged out succussfully!'
   end
 end
