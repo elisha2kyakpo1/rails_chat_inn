c2c1e1580d01ea35a5c7fabc05baead0d76a7ff8
elisha2kyakpo1 <kyakopoe@gmail.com>
2022-03-08 14:40:59 +0200

    Add associations to room and user

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,7 @@
 class Room < ApplicationRecord
+  belongs_to :user
   validates :name, presence: true
+  has_rich_text :contents
   scope :public_rooms, -> { where(is_private: false) }
   scope :private_rooms, -> { where(is_private: true) }
 end
