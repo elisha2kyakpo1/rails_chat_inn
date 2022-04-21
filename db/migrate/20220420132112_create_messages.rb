b10cd799f18565be95f8acaf71ee2b29f6e7e15d
elisha2kyakpo1 <kyakopoe@gmail.com>
2022-04-21 15:31:14 +0200

    Add messages controller

--- db/migrate/20220420132112_create_messages.rb
+++ db/migrate/20220420132112_create_messages.rb
@@ -3,7 +3,6 @@ class CreateMessages < ActiveRecord::Migration[7.0]
     create_table :messages do |t|
       t.references :user, null: false, foreign_key: true
       t.references :room, null: false, foreign_key: true
-      t.text :body
 
       t.timestamps
     end
