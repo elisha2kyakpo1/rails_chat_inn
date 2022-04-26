695a7cd61453d365bc7ba0c03d3f28cf152e9171
elisha2kyakpo1 <kyakopoe@gmail.com>
2022-04-26 13:20:32 +0200

    Fix styling to the link_to

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,6 @@ Rails.application.routes.draw do
   resources :rooms do
     resources :messages
   end
-  devise_for :users do
-    delete '/sign_out' => 'devise/sessions#destroy', :as => :destroy_user_session
-  end
+  devise_for :users
   resources :users
 end
