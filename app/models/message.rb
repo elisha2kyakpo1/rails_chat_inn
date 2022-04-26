9a7a44023f6ab468e264148336acf8c363027478
elisha2kyakpo1 <kyakopoe@gmail.com>
2022-04-26 16:00:37 +0200

    Fix rubocop errors

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,13 +1,13 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
-  after_create_commit { broadcast_append_to self.room }
+  after_create_commit { broadcast_append_to room }
   before_create :confirm_participant
 
   def confirm_participant
-    return unless self.room.is_private
+    return unless room.is_private
 
-      is_participant = Participant.where(user_id: self.user.id, room_id: self.room.id).first
-      throw :abort unless is_participant
+    is_participant = Participant.where(user_id: user.id, room_id: room.id).first
+    throw :abort unless is_participant
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable
 
   validates_uniqueness_of :name
   scope :all_except, ->(user) { where.not(id: user) }
