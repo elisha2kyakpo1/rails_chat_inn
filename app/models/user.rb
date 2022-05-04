22af8a73675c418a159ab2fe4feb2c03c31b0727
elisha2kyakpo1 <kyakopoe@gmail.com>
2022-05-04 14:48:53 +0200

    Add styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,17 +12,6 @@ class User < ApplicationRecord
 
   has_many :messages, dependent: :destroy
   has_one :room
-  has_one_attached :pfp
-
-  after_commit :add_default_pfp, on: %i[create update]
-
-  def pfp_thumbnail
-    pfp.variant(resize_to_limit: [150, 150]).processed
-  end
-
-  def chat_pfp
-    pfp.variant(resize_to_limit: [50, 50]).processed
-  end
 
   def status_to_css
     case status
@@ -41,15 +30,5 @@ class User < ApplicationRecord
 
   def broadcast_update
     broadcast_replace_to 'user_status', partial: 'users/status', user: self
-  end
-
-  def add_default_pfp
-    return if pfp.attached?
-
-    pfp.attach(
-      io: File.open(Rails.root.join('app', 'assets', 'images', 'default_profile.jpg')),
-      filename: 'default_profile.jpg',
-      content_type: 'image/png'
-    )
   end
 end
