fea80488b6dc56c1c1f17029a877eed3350c0060
elisha2kyakpo1 <kyakopoe@gmail.com>
2022-08-12 12:47:27 +0200

    Add avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -74,6 +74,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_one_attached :avatar
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
