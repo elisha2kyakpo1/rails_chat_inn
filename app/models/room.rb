5fc2c8b57e4c9230f5c8ec83b5d80e53ebf4568e
elisha2kyakpo1 <kyakopoe@gmail.com>
2022-08-16 12:37:37 +0200

    Add icons

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,6 @@
 class Room < ApplicationRecord
   validates_uniqueness_of :name
+  validates :name, presence: true
   scope :public_rooms, -> { where(is_private: false) }
   after_create_commit { broadcast_if_public }
 
