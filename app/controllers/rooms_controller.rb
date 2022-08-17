6aea878502dc4d34ce42e027ab3cb4ed2ce284fc
elisha2kyakpo1 <kyakopoe@gmail.com>
2022-08-17 16:55:58 +0200

    Add description to rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -23,6 +23,6 @@ class RoomsController < ApplicationController
   end
 
   def create
-    @room = Room.create(name: params['room']['name'])
+    @room = Room.create(name: params['room']['name'], description: params['room']['description'], is_private: false)
   end
 end
